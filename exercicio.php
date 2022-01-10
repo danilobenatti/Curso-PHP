4f9b179ac85592ff1c22a7175a234e217a6de53a
Danilo N. B. <danilobenatti@gmail.com>
2022-01-10 07:38:08 -0300

    Projeto Curso de PHP.

---
+++ exercicio.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+<link
+	href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+	rel="stylesheet">
+<link rel="stylesheet" href="resources\css\style.css">
+<link rel="stylesheet" href="resources\css\exercicio.css">
+<title>Exercício</title>
+</head>
+<body class="exercicio">
+	<header class="cabecalho">
+		<h1>Curso PHP</h1>
+		<h2>Visualização do Exercício</h2>
+	</header>
+	<nav class="navegacao">
+		<a href=<?="{$_GET['dir']}/{$_GET['file']}.php"?> class="verde">Sem formatação</a>
+		<a href="index.php" class="vermelho">Voltar</a>
+	</nav>
+	<main class="principal">
+		<div class="conteudo">
+			<?php
+				//include($_GET['dir'] . "/" . $_GET['file'] . ".php");
+				// {$_GET['dir']}
+				// {$_GET['file']}
+				include(__DIR__."/{$_GET['dir']}/{$_GET['file']}.php");
+			?>
+		</div>
+	</main>
+	<footer class="rodape">COD3R &amp ALUNOS &copy <?= date('Y')?></footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+<link
+	href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+	rel="stylesheet">
+<link rel="stylesheet" href="resources\css\style.css">
+<title>Curso PHP</title>
+</head>
+<body>
+	<header class="cabecalho">
+		<h1>Curso PHP</h1>
+		<h2>Índice do Conteúdo</h2>
+	</header>
+	<main class="principal">
+		<div class="conteudo">
+			<nav class="capitulos">
+				<div class="secoes verde">
+					<h3>Capítulo 1</h3>
+					<ul>
+						<li><a href="exercicio.php?dir=teste&file=teste">Seção A</a></li>
+						<li><a href="#">Seção B</a></li>
+						<li><a href="#">Seção C</a></li>
+					</ul>
+				</div>
+				<div class="secoes vermelho">
+					<h3>Capítulo 2</h3>
+					<ul>
+						<li><a href="#">Seção A</a></li>
+						<li><a href="#">Seção B</a></li>
+						<li><a href="#">Seção C</a></li>
+					</ul>
+				</div>
+				<div class="secoes azul">
+					<h3>Capítulo 3</h3>
+					<ul>
+						<li><a href="#">Seção A</a></li>
+						<li><a href="#">Seção B</a></li>
+						<li><a href="#">Seção C</a></li>
+					</ul>
+				</div>
+				<div class="secoes roxo">
+					<h3>Capítulo 4</h3>
+					<ul>
+						<li><a href="#">Seção A</a></li>
+						<li><a href="#">Seção B</a></li>
+						<li><a href="#">Seção C</a></li>
+					</ul>
+				</div>
+				<div class="secoes laranja">
+					<h3>Capítulo 5</h3>
+					<ul>
+						<li><a href="#">Seção A</a></li>
+						<li><a href="#">Seção B</a></li>
+						<li><a href="#">Seção C</a></li>
+					</ul>
+				</div>
+				<div class="secoes verde-escuro">
+					<h3>Capítulo 6</h3>
+					<ul>
+						<li><a href="#">Seção A</a></li>
+						<li><a href="#">Seção B</a></li>
+						<li><a href="#">Seção C</a></li>
+					</ul>
+				</div>
+				<div class="secoes vermelho-escuro">
+					<h3>Capítulo 7</h3>
+					<ul>
+						<li><a href="#">Seção A</a></li>
+						<li><a href="#">Seção B</a></li>
+						<li><a href="#">Seção C</a></li>
+					</ul>
+				</div>
+				<div class="secoes azul-escuro">
+					<h3>Capítulo 8</h3>
+					<ul>
+						<li><a href="#">Seção A</a></li>
+						<li><a href="#">Seção B</a></li>
+						<li><a href="#">Seção C</a></li>
+					</ul>
+				</div>
+				<div class="secoes roxo-escuro">
+					<h3>Capítulo 9</h3>
+					<ul>
+						<li><a href="#">Seção A</a></li>
+						<li><a href="#">Seção B</a></li>
+						<li><a href="#">Seção C</a></li>
+					</ul>
+				</div>
+				<div class="secoes laranja-escuro">
+					<h3>Capítulo 10</h3>
+					<ul>
+						<li><a href="#">Seção A</a></li>
+						<li><a href="#">Seção B</a></li>
+						<li><a href="#">Seção C</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</main>
+	<footer class="rodape">COD3R &amp ALUNOS &copy <?= date('Y')?></footer>
+</body>
+</html>
