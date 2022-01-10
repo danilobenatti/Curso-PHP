d8667c3b399ffc2bbeca7776bf53a2bb4872ddfe
Danilo N. B. <danilobenatti@gmail.com>
2022-01-10 08:58:20 -0300

    Olá PHP

---
+++ basico/ola.php
@@ -0,0 +1,13 @@
+<div class="titulo">Olá PHP</div>
+
+<h3>Exemplo A</h3>
+<?php 
+echo 'Olá ';
+echo "Mundo PHP!";
+?>
+
+<h3>Exemplo B</h3>
+<?= "Outra forma de me 'expressar'!" ?>
+<?= 2 * 3 + 3 ?>
+
+<?php phpinfo();?>
