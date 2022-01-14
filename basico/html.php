126f03dd887a3d887c3175f446c51574bc933c95
Danilo N. B. <danilobenatti@gmail.com>
2022-01-14 08:21:16 -0300

    Integração HTML

---
+++ basico/html.php
@@ -0,0 +1,10 @@
+<div class="titulo">Integração HTML</div>
+<h1>
+<?php 
+	echo 'Olá';
+	echo '<small> Mundo!</small>';
+?>
+</h1>
+<?= "<div>Outra forma de me 'expressar'!</div>"; ?>
+<br>
+<div><button><?= "Legal"?></button></div>
