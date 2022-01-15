746085a2499aa8f0c55f2b8bd786b01f285a72da
Danilo N. B. <danilobenatti@gmail.com>
2022-01-15 08:01:50 -0300

    Tipo Inteiro

--- index.php
+++ index.php
@@ -19,13 +19,19 @@
 		<div class="conteudo">
 			<nav class="capitulos">
 				<div class="secoes verde">
-					<h3>Capítulo 1</h3>
+					<h3>Capítulo 1 - Básico</h3>
 					<ul>
-						<li><a href="exercicio.php?dir=basico&file=ola">Olá PHP!</a></li>
-						<li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
-						<li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
-						<li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
-						<li><a href="exercicio.php?dir=basico&file=desafio">Desafio do capítulo</a></li>
+						<li><a href="exercicio.php?dir=basico&file=ola"> Olá PHP! </a></li>
+						<li><a href="exercicio.php?dir=basico&file=html"> Integração HTML</a></li>
+						<li><a href="exercicio.php?dir=basico&file=css"> Integração CSS </a></li>
+						<li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP </a></li>
+						<li><a href="exercicio.php?dir=basico&file=desafio"> Desafio do capítulo </a></li>
+					</ul>
+				</div>
+				<div class="secoes vermelho">
+					<h3>Capítulo 2 - Tipos</h3>
+					<ul>
+						<li><a href="exercicio.php?dir=tipos&file=int"> Inteiros </a></li>
 					</ul>
 				</div>
 			</nav>
---
+++ tipos/int.php
@@ -0,0 +1,17 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+echo 11;
+echo '<br>';
+
+var_dump(11);
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>';
+echo PHP_INT_MIN, '<br>';
+echo 017, '<br>'; //base octal
+echo 020, '<br>'; //base octal
+echo 0b1111, '<br>'; //base binária
+echo 0b10000, '<br>'; //base binária
+echo 0x0f, '<br>'; //base hexadecimal
+echo 0x10, '<br>'; //base hexadecimal
