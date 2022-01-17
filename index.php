b45aabcf8aa534462945f01cd99c48a80ffb1db1
Danilo N. B. <danilobenatti@gmail.com>
2022-01-16 23:21:46 -0300

    Desafio Precedência

--- index.php
+++ index.php
@@ -21,19 +21,20 @@
 				<div class="secoes verde">
 					<h3>Capítulo 1 - Básico</h3>
 					<ul>
-						<li><a href="exercicio.php?dir=basico&file=ola"> Olá PHP! </a></li>
-						<li><a href="exercicio.php?dir=basico&file=html"> Integração HTML</a></li>
-						<li><a href="exercicio.php?dir=basico&file=css"> Integração CSS </a></li>
-						<li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP </a></li>
-						<li><a href="exercicio.php?dir=basico&file=desafio"> Desafio do capítulo </a></li>
+						<li><a href="exercicio.php?dir=basico&file=ola">Olá PHP!</a></li>
+						<li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+						<li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+						<li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+						<li><a href="exercicio.php?dir=basico&file=desafio">Desafio do capítulo</a></li>
 					</ul>
 				</div>
 				<div class="secoes vermelho">
 					<h3>Capítulo 2 - Tipos</h3>
 					<ul>
-						<li><a href="exercicio.php?dir=tipos&file=int"> Inteiros </a></li>
-						<li><a href="exercicio.php?dir=tipos&file=float"> Floats </a></li>
-						<li><a href="exercicio.php?dir=tipos&file=aritmeticas"> Operações Aritméticas </a></li>
+						<li><a href="exercicio.php?dir=tipos&file=int">Inteiros</a></li>
+						<li><a href="exercicio.php?dir=tipos&file=float">Floats</a></li>
+						<li><a href="exercicio.php?dir=tipos&file=aritmeticas">Operações Aritméticas</a></li>
+						<li><a href="exercicio.php?dir=tipos&file=desafio_precedencia">Desafio Precedência</a></li>
 					</ul>
 				</div>
 			</nav>
---
+++ tipos/desafio_precedencia.php
@@ -0,0 +1,12 @@
+<div class="titulo">Desafio Precedência</div>
+
+<?php
+	echo '<p>. Qual o valor será impresso?</p>';
+	echo 2 + 5 * 3 + (12 / 6) / (-8 + 2 ** 3); // INF
+	
+	echo '<p>. Qual expressão imprime o valor 100?</p>';
+	echo 'a)', (1 + 2) * 20 - 15, '<br>'; // 45
+	echo 'b)', 4 * 5 ** 2, '<br>'; // 100
+	echo 'c)', 2 ** 3 ** 4 / 1e25, '<br>'; // 0.24178516392293
+	echo 'd)', 3 + (3 * 8) / 2 - 3, '<br>'; // 10
+?>
