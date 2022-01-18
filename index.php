e802ea7a58999df2b87b165519c6a5f1e5f3c1b8
danilobenatti <danilobenatti@gmail.com>
2022-01-18 16:28:58 -0300

    Variáveis variáveis

--- index.php
+++ index.php
@@ -48,6 +48,7 @@
 						<li><a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio Equação</a></li>
 						<li><a href="exercicio.php?dir=variaveis&file=atribuicoes">Atribuições</a></li>
 						<li><a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</a></li>
+						<li><a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">Variáveis variáveis</a></li>
 					</ul>
 				</div>
 			</nav>
