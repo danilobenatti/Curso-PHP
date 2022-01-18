d0cc4088f2dc0a4df60ae4ed4f82ec4c96dcce14
danilobenatti <danilobenatti@gmail.com>
2022-01-18 15:04:33 -0300

    Interpolação

--- index.php
+++ index.php
@@ -47,6 +47,7 @@
 						<li><a href="exercicio.php?dir=variaveis&file=basico">Básico</a></li>
 						<li><a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio Equação</a></li>
 						<li><a href="exercicio.php?dir=variaveis&file=atribuicoes">Atribuições</a></li>
+						<li><a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</a></li>
 					</ul>
 				</div>
 			</nav>
