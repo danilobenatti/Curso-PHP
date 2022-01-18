d6a3a3056bf8ad993bd2654d65e3fee8ed7b8e88
danil <danil@DESKTOP>
2022-01-18 11:57:16 -0300

    Variáveis #02

--- variaveis/basico.php
+++ variaveis/basico.php
@@ -25,4 +25,21 @@
 	$variavel = 'Agora é uma string';
 	echo '<br>' . $variavel;
 	echo '<br>', var_dump($variavel);
+	
+	//Nome de variável
+	$var = "é válida";
+	echo '<br>'. $var;
+	$var2 = 'é válida';
+	echo '<br>'. $var2;
+	$VAR3 = 'é válida';
+	echo '<br>'. $VAR3;
+	$_var_4 = 'é válida';
+	echo '<br>'. $_var_4;
+	$vâr5 = 'é válida'; //não aconselhado. evitar!
+	echo '<br>'. $vâr5; //não aconselhado. evitar!
+// 	$6var = 'inválido';
+// 	$%var7 = 'inválio';
+// 	$var8% = 'inválido';
+	
+	echo '<br>', var_dump($_SERVER["HTTP_HOST"]);
 ?>
