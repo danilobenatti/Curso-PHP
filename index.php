c0e1b2023e042ab754e0822bc7f303513481960a
Danilo N. B. <danilobenatti@gmail.com>
2022-02-15 01:11:49 -0300

    Usando $_GET

--- index.php
+++ index.php
@@ -78,6 +78,7 @@
 						<li><a href="exercicio.php?dir=array&file=desafio_sorteio">Desafio Sorteio</a></li>
 						<li><a href="exercicio.php?dir=array&file=array_multidimensional">Arrays Multidimensionais</a></li>
 						<li><a href="exercicio.php?dir=array&file=array_constantes">Arrays Constantes</a></li>
+						<li><a href="exercicio.php?dir=array&file=get">$_GET</a></li>
 					</ul>
 				</div>
 			</nav>
