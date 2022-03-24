3a2cfc8a3e0657aab5194a52575530d4c6a4be59
Danilo N. B. <danilobenatti@gmail.com>
2022-03-24 11:53:52 -0300

    Traits #01

--- index.php
+++ index.php
@@ -126,6 +126,7 @@
 						<li><a href="exercicio.php?dir=classes_objetos&file=abstract">Classe Abstrata</a></li>
 						<li><a href="exercicio.php?dir=classes_objetos&file=desafio_erros">Desafio Erros</a></li>
 						<li><a href="exercicio.php?dir=classes_objetos&file=modificador_final">Modificador &apos;final&apos;</a></li>
+						<li><a href="exercicio.php?dir=classes_objetos&file=traits_01">Traits #01</a></li>
 					</ul>
 				</div>
 			</nav>
